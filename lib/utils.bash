07b87fe24c159b2bca6dc03e41354dee6a3534d2
Kenichi Kamiya <kachick1@gmail.com>
2023-05-24 17:19:40 +0900

    Prefer tab indent for bash files
    To follow https://github.com/asdf-vm/asdf-plugin-template/commit/dd6a49f3506d807373a00a4b5cf3801b5ae8be3b#diff-81f762efdc41323cb8113d5930b4c7d897fcf38134afbcb510815ad7a96df24bR3

--- lib/utils.bash
+++ lib/utils.bash
@@ -12,104 +12,104 @@ TOOL_TEST="cargo-make make --version"
 supporting_cargomake_version_pattern='^([1-9][0-9]{0,}|0)\.([2-9][0-9]|1[6-9])\.[0-9]{1,}$'
 
 filter_supporting_cargomake_versions() {
-  grep -E "$supporting_cargomake_version_pattern"
+	grep -E "$supporting_cargomake_version_pattern"
 }
 
 is_supporting_cargomake_version() {
-  echo "$ASDF_INSTALL_VERSION" | grep -qE "$supporting_cargomake_version_pattern"
+	echo "$ASDF_INSTALL_VERSION" | grep -qE "$supporting_cargomake_version_pattern"
 }
 
 fail() {
-  echo -e "asdf-$TOOL_NAME: $*"
-  exit 1
+	echo -e "asdf-$TOOL_NAME: $*"
+	exit 1
 }
 
 curl_opts=(-fsSL)
 
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
 sort_versions() {
-  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
 list_github_tags() {
-  git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+	git ls-remote --tags --refs "$GH_REPO" |
+		grep -o 'refs/tags/.*' | cut -d/ -f3- |
+		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
 list_all_versions() {
-  list_github_tags
+	list_github_tags
 }
 
 list_all_supporting_versions() {
-  list_all_versions | filter_supporting_cargomake_versions
+	list_all_versions | filter_supporting_cargomake_versions
 }
 
 download_release() {
-  local version filename url
-  version="$1"
-  filename="$2"
+	local version filename url
+	version="$1"
+	filename="$2"
 
-  local platform
+	local platform
 
-  case "$OSTYPE" in
-  darwin*) platform="apple-darwin" ;;
-  linux*) platform="unknown-linux-musl" ;;
-  *) fail "Unsupported platform" ;;
-  esac
+	case "$OSTYPE" in
+	darwin*) platform="apple-darwin" ;;
+	linux*) platform="unknown-linux-musl" ;;
+	*) fail "Unsupported platform" ;;
+	esac
 
-  local architecture
+	local architecture
 
-  case "$(uname -m)" in
-  x86_64*) architecture="x86_64" ;;
-  aarch64 | arm64) fail "Unsupported architecture for now. See https://github.com/sagiegurari/cargo-make/issues/738" ;;
-  *) fail "Unsupported architecture" ;;
-  esac
+	case "$(uname -m)" in
+	x86_64*) architecture="x86_64" ;;
+	aarch64 | arm64) fail "Unsupported architecture for now. See https://github.com/sagiegurari/cargo-make/issues/738" ;;
+	*) fail "Unsupported architecture" ;;
+	esac
 
-  local archive_format="zip"
+	local archive_format="zip"
 
-  # They use this naming since 0.15.1, currently do not work for the ancient versions
-  # https://github.com/sagiegurari/cargo-make/releases/tag/0.15.1 - 2018-11-13
-  # https://github.com/sagiegurari/cargo-make/releases/tag/0.15.0 - 2018-11-13
-  # Snapshot of the addressies are below
-  # https://github.com/sagiegurari/cargo-make/releases/download/0.36.2/cargo-make-v0.36.2-x86_64-unknown-linux-musl.zip
-  # https://github.com/sagiegurari/cargo-make/releases/download/0.36.2/cargo-make-v0.36.2-x86_64-apple-darwin.zip
-  # https://github.com/sagiegurari/cargo-make/releases/download/0.36.2/cargo-make-v0.36.2-x86_64-pc-windows-msvc.zip
-  local url="$GH_REPO/releases/download/${version}/cargo-make-v${version}-${architecture}-${platform}.${archive_format}"
+	# They use this naming since 0.15.1, currently do not work for the ancient versions
+	# https://github.com/sagiegurari/cargo-make/releases/tag/0.15.1 - 2018-11-13
+	# https://github.com/sagiegurari/cargo-make/releases/tag/0.15.0 - 2018-11-13
+	# Snapshot of the addressies are below
+	# https://github.com/sagiegurari/cargo-make/releases/download/0.36.2/cargo-make-v0.36.2-x86_64-unknown-linux-musl.zip
+	# https://github.com/sagiegurari/cargo-make/releases/download/0.36.2/cargo-make-v0.36.2-x86_64-apple-darwin.zip
+	# https://github.com/sagiegurari/cargo-make/releases/download/0.36.2/cargo-make-v0.36.2-x86_64-pc-windows-msvc.zip
+	local url="$GH_REPO/releases/download/${version}/cargo-make-v${version}-${architecture}-${platform}.${archive_format}"
 
-  echo "* Downloading $TOOL_NAME release $version..."
-  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+	echo "* Downloading $TOOL_NAME release $version..."
+	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
 install_version() {
-  local install_type="$1"
-  local version="$2"
-  local install_path="${3%/bin}/bin"
+	local install_type="$1"
+	local version="$2"
+	local install_path="${3%/bin}/bin"
 
-  if [ "$install_type" != "version" ]; then
-    fail "asdf-$TOOL_NAME supports release installs only"
-  fi
+	if [ "$install_type" != "version" ]; then
+		fail "asdf-$TOOL_NAME supports release installs only"
+	fi
 
-  (
-    mkdir -p "$install_path"
+	(
+		mkdir -p "$install_path"
 
-    # cargo-make has LICENSE and README.md in same dir, and recursivie copy for each file makes unexpected shims.
-    # So DO NOT USE `cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"` here.
-    # And `makers` command does not exist in old versions
-    cp "${ASDF_DOWNLOAD_PATH}/cargo-make" "$install_path"
-    cp "${ASDF_DOWNLOAD_PATH}/makers" "$install_path" || echo "Version $version does not have 'makers' command"
+		# cargo-make has LICENSE and README.md in same dir, and recursivie copy for each file makes unexpected shims.
+		# So DO NOT USE `cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"` here.
+		# And `makers` command does not exist in old versions
+		cp "${ASDF_DOWNLOAD_PATH}/cargo-make" "$install_path"
+		cp "${ASDF_DOWNLOAD_PATH}/makers" "$install_path" || echo "Version $version does not have 'makers' command"
 
-    local tool_cmd
-    tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-    test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		local tool_cmd
+		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
-    echo "$TOOL_NAME $version installation was successful!"
-  ) || (
-    rm -rf "$install_path"
-    fail "An error occurred while installing $TOOL_NAME $version."
-  )
+		echo "$TOOL_NAME $version installation was successful!"
+	) || (
+		rm -rf "$install_path"
+		fail "An error occurred while installing $TOOL_NAME $version."
+	)
 }
--- scripts/lint.bash
+++ scripts/lint.bash
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 shellcheck --shell=bash --external-sources \
-  bin/* --source-path=template/lib/ \
-  lib/* \
-  scripts/*
+	bin/* --source-path=template/lib/ \
+	lib/* \
+	scripts/*
 
 shfmt --language-dialect bash --diff \
-  ./**/*
+	./**/*
 
-dprint check
+dprint check \
+	fff
