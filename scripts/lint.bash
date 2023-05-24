99ebd4656fa8bff37a23c757e74716e23230d704
Kenichi Kamiya <kachick1@gmail.com>
2023-05-24 17:08:38 +0900

    Follow updated template
    https://github.com/asdf-vm/asdf-plugin-template/commit/dd6a49f3506d807373a00a4b5cf3801b5ae8be3b#diff-81f762efdc41323cb8113d5930b4c7d897fcf38134afbcb510815ad7a96df24bR3

---
+++ scripts/lint.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+shellcheck --shell=bash --external-sources \
+  bin/* --source-path=template/lib/ \
+  lib/* \
+  scripts/*
+
+shfmt --language-dialect bash --diff \
+  ./**/*
+
+dprint check
