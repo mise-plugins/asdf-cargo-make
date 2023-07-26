f4c7a5d47047f5c953bc2961ca0c76b5e299dff6
Kenichi Kamiya <kachick1@gmail.com>
2023-07-26 18:10:46 +0900

    Prefer to depend typos with asdf (#84)
    * `asdf local typos latest`
    
    * Prefer to depend typos with asdf
    
    * Fix commit miss
    
    * Log executed commands in lint/fmt

--- scripts/format.bash
+++ scripts/format.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euxo pipefail
+
 shfmt --language-dialect bash --write \
 	./**/*
 
--- scripts/lint.bash
+++ scripts/lint.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euxo pipefail
+
 shellcheck --shell=bash --external-sources \
 	bin/* --source-path=lib/ \
 	lib/* \
@@ -9,3 +11,5 @@ shfmt --language-dialect bash --diff \
 	./**/*
 
 dprint check
+
+typos . .github .vscode
